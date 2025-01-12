8377558a357f910c976b930bebe994536dad5b9f
Rajneesh Shukla <44999300+rajneesh18@users.noreply.github.com>
2025-01-12 11:10:32 +0000

    let const & var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 291
+let accountEmail = "rkshukla@gmail.com"
+var accountPassword = "12345"
+accountState = "New Delhi"
+let accountCity;
+
+// accountId = 201  // Not Allowed
+
+accountEmail = "rk2596@gmail.com"
+accountPassword = "9873434"
+accountState = "Uttar Pradesh"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
