4ca6e802c752570f938dbc380c7d89ff83ac767f
Rajneesh Shukla <44999300+rajneesh18@users.noreply.github.com>
2025-01-12 11:54:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Rajneesh");
+
+let name = "Rajneeesh"
+let age = 22
+let isLoggedIn = false
+
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// Object
+
+console.log(typeof "rajneesh") // string
+console.log(typeof null);   // object
+console.log(typeof undefined); // undefined
+
